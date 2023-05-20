885e6c8942cf02ca3a9ef35f7e194d3a438b4b61
Naimur Rahman <nevergiveupnr.tech@gmail.com>
2023-05-21 01:28:51 +0600

    up to date

--- app/src/test/java/com/example/cardiacrecorder/ExampleUnitTest.java
+++ app/src/test/java/com/example/cardiacrecorder/ExampleUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.example.cardiacrecorder.classes.EachData;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +15,11 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void addData(){
+        EachData datalist = new EachData();
+        
     }
 }
